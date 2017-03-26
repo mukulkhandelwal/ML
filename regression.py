83fe68b28a1de56d1efb0cda662f11c340e93b36
Mukul <you@example.com>
2017-03-27 00:29:39 +0530

    forecasting and predicting

--- regression.py
+++ regression.py
@@ -1,5 +1,5 @@
 #Regression Intro
-
+from datetime import datetime
 
 import pandas as pd
 import  quandl
@@ -7,7 +7,10 @@ import math
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm 
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
 
+style.use('ggplot')
 
 quandl.ApiConfig.api_key = 'QM1RfC8UNPe6_AqSSde4'
 quandl.ApiConfig.api_version = '2015-04-09'
@@ -35,11 +38,16 @@ df.dropna(inplace=True)
 #print(df.head())
 
 X = np.array(df.drop(['label'],1)) #features except label
-y = np.array(df['label'])
-print(y)
-X = preprocessing.scale(X) #scale the values
-print(X)
+ #scale the values
+
+X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
+#print(X)
+
 df.dropna(inplace=True)
+#y = np.array(df['label'])
 y = np.array(df['label'])
 
 #print(len(X),len(y))
@@ -50,6 +58,27 @@ clf = LinearRegression() #LinearRegression(n_jobs=10) 10 times training data  # 
 clf.fit(X_train, y_train)  #train
 accuracy = clf.score(X_test, y_test)  #test
 
-print(accuracy)
+#print(accuracy)
+
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy,forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.ilox[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
 
 
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1) ]+ [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+
+plt.legend(loc = 4)
+plt.xlabel('Date')
+plt.ylabel()
