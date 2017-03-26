db3eaefc454d3447907ea4093d633156c200da16
Mukul <you@example.com>
2017-03-26 11:00:03 +0530

    error solved

--- regression.py
+++ regression.py
@@ -1,18 +1,21 @@
 #Regression Intro
 
 import pandas as pd
-import  Quandl
+import  quandl
 
-df = Quandl.get('WIKI/GOOGL')
+quandl.ApiConfig.api_key = 'QM1RfC8UNPe6_AqSSde4'
+quandl.ApiConfig.api_version = '2015-04-09'
 
-#df = df[['Adj.Open','Adj.High','Adj.Low','Adj.Close','Adj.Volume']]
+df = quandl.get('WIKI/GOOGL')
+#print(df)
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
 
-df['HL_PCT'] = (df['Adj.High'] - df['Adj.Close'])/df['Adj.Close'] *100.0
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close'])/df['Adj. Close'] *100.0
 
-df['PCT_change'] = (df['Adj.Close'] - df['Adj.Open'])/df['Adj.Open'] *100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open'])/df['Adj. Open'] *100.0
 
-df = df[['Adj.Close','HL_PCT','PCT_change','Adj.Volume']]
-
+df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
+print(df)
 
 
 
