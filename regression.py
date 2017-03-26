0bf5646d7d0600255c29160613060d0e47914d4c
Mukul <you@example.com>
2017-03-26 11:23:21 +0530

    features and labels

--- regression.py
+++ regression.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import  quandl
+import math
 
 quandl.ApiConfig.api_key = 'QM1RfC8UNPe6_AqSSde4'
 quandl.ApiConfig.api_version = '2015-04-09'
@@ -15,7 +16,22 @@ df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close'])/df['Adj. Close'] *100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open'])/df['Adj. Open'] *100.0
 
 df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
-print(df)
+#print(df)
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace = True) #nan data
+
+forecast_out = int(math.ceil(0.01*len(df)))  
+
+
+#labels
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+print(df.head())
+
+
+
+
 
 
 
