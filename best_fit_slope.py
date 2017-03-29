8603c412be3e35a94e54f2b6b14b2e48c5555ffd
Mukul <you@example.com>
2017-03-30 01:01:45 +0530

    find b and complete best fit

--- best_fit_slope.py
+++ best_fit_slope.py
@@ -7,12 +7,20 @@ xs = np.array([1, 2, 3, 4, 5, 6],dtype=float)
 ys = np.array([5, 4, 6, 5, 6, 7], dtype=float)
 
 
-def best_fit_slope(xs,ys):
+def best_fit_slope_and_intercept(xs,ys):
     m = ( ( (mean(xs) * mean(ys)) - mean(xs*ys) ) / ( (mean(xs)**2) - mean(xs**2) ))  #xs^2 square
-    return m
+    b = mean(ys) - m*mean(xs)
+    return m, b
 
-m  = best_fit_slope(xs,ys)
-print(m)
+m,b  = best_fit_slope_and_intercept(xs,ys)
+print(m, b)
 
-# plt.scatter(xs,ys)
-# plt.show()
+
+regression_line = [(m*x)+b for x in xs]
+predict_x = 8
+predict_y = (m*predict_x) + b
+
+plt.scatter(xs,ys)
+plt.scatter(predict_x,predict_y)
+plt.plot(xs,regression_line)
+plt.show()
