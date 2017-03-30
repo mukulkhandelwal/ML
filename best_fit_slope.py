98b85ebde8bdaebf9619b966fee9ae2bbe1f2195
Mukul <you@example.com>
2017-03-30 18:14:30 +0530

    Testing assumption

--- best_fit_slope.py
+++ best_fit_slope.py
@@ -2,11 +2,29 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import  style
+import random
 
 style.use('fivethirtyeight')
 
-xs = np.array([1, 2, 3, 4, 5, 6],dtype=float)
-ys = np.array([5, 4, 6, 5, 6, 7], dtype=float)
+#xs = np.array([1, 2, 3, 4, 5, 6],dtype=float)
+#ys = np.array([5, 4, 6, 5, 6, 7], dtype=float)
+
+
+def create_dataset(hm, variance, step = 2, correlation = False):  #hm =how much dataset
+    val = 1
+    ys = []
+    for i in range(hm):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val+=step
+        elif correlation and correlation == 'neg':
+            val -=step
+
+    xs = [i for i in range(len(ys))]
+
+    return np.array(xs, dtype=np.float), np.array(ys, dtype=float)
+
 
 
 def best_fit_slope_and_intercept(xs,ys):
@@ -26,6 +44,8 @@ def coefficient_of_determination(ys_orig, ys_line):
     return (1- (squared_error_regr / squared_error_y_mean))
 
 
+xs, ys = create_dataset(40, 40, 2, correlation='pos')
+
 m,b  = best_fit_slope_and_intercept(xs,ys)
 #print(m, b)
 
@@ -42,3 +62,11 @@ plt.scatter(predict_x,predict_y)
 #plt.plot(xs,ys)
 plt.plot(xs,regression_line)
 plt.show()
+
+
+
+
+
+
+
+
