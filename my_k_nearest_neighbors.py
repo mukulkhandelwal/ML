36e327bbbdbb2fe066e203d2712a8e62f58a4e78
Mukul <you@example.com>
2017-04-01 16:36:16 +0530

    p18

--- my_k_nearest_neighbors.py
+++ my_k_nearest_neighbors.py
@@ -1,20 +1,21 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import style
+# import matplotlib.pyplot as plt
+# from matplotlib import style
 from math import sqrt
 import warnings
 from collections import Counter
-
+import pandas as pd
+import random
 # plot1 = [1,3]
 # plot2 = [2,5]
 #euclidean_distance = sqrt( ( plot1[0]-plot2[0])** 2 + (plot1[1] - plot2[1]) ** 2)
 #print(euclidean_distance)
 
 
-style.use('fivethirtyeight')
-
-dataset = {'k' : [[1,2] ,[2,3], [3,1]], 'r' : [[6,5], [7,7], [8,6]] }
-new_feature = [5,7]
+#style.use('fivethirtyeight')
+#
+# dataset = {'k' : [[1,2] ,[2,3], [3,1]], 'r' : [[6,5], [7,7], [8,6]] }
+# new_feature = [5,7]
 
 #
 # for i in dataset:
@@ -35,7 +36,7 @@ def k_nearest_neighbors(data, predict, k=3):
             distances.append([euclidean_distance, group])
 
     votes = [i[1] for i in sorted(distances)[:k]] #finding groups
-    print(Counter(votes).most_common(1))
+    #print(Counter(votes).most_common(1))
     #
     # for i in sorted(distances)[:k]:  #same as above
     #     i[i]
@@ -45,10 +46,44 @@ def k_nearest_neighbors(data, predict, k=3):
     return vote_result
 
 
-result = k_nearest_neighbors(dataset ,new_feature, k=3)
-print(result)
+# result = k_nearest_neighbors(dataset ,new_feature, k=3)
+# print(result)
+#
+# [[plt.scatter(ii[0], ii[1], s=100, color = i)for ii in dataset[i]] for i in dataset]
+# plt.scatter(new_feature[0], new_feature[1], color = result)
+# plt.show()
 
-[[plt.scatter(ii[0], ii[1], s=100, color = i)for ii in dataset[i]] for i in dataset]
-plt.scatter(new_feature[0], new_feature[1], color = result)
-plt.show()
+df = pd.read_csv('breast-cancer-wisconsin.data')
+df.replace('?',-99999,inplace = True)
 
+df.drop(['id'],1,inplace = True)
+#print(df.head())
+full_data = df.astype(float).values.tolist()  #convert all to float there are some string thats why convert to float
+#print(full_data[:5])
+
+random.shuffle(full_data)
+#print(full_data[:5])
+
+test_size = 0.2
+train_set = {2:[], 4:[]}
+test_set = {2:[], 4:[] }
+train_data = full_data[: -int(test_size*len(full_data))]
+test_data = full_data[-int(test_size*len(full_data)):] #last 20%
+
+
+for i in train_data:
+    train_set[i[-1]].append(i[:-1]) #-1 is last column which is class column
+for i in test_data:
+    test_set[i[-1]].append(i[:-1]) #-1 is last column which is class column
+
+correct = 0
+total = 0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote = k_nearest_neighbors(train_set, data, k=5)
+        if group == vote :
+            correct +=1
+        total+=1
+
+print('Accuracy: ', correct/total)
