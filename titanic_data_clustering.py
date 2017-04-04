10fb580a19e2fb6adaa27592507fef1b54b8a4b6
Mukul <mukulkhandelwal1997@gmail.com>
2017-04-04 12:47:27 +0530

    K means with Titanic Dataset p36

--- titanic_data_clustering.py
+++ titanic_data_clustering.py
@@ -4,28 +4,28 @@ from matplotlib import style
 style.use('ggplot')
 import numpy as np
 from sklearn.cluster import KMeans
-from sklearn import preprocessing,cross_validation
+from sklearn import preprocessing
 
 import pandas as pd
 #import xlrd
 
+'''
+Pclass Passenger Class (1 = 1st; 2 = 2nd; 3 = 3rd)
+survival Survival (0 = No; 1 = Yes)
+name Name
+sex Sex
+age Age
+sibsp Number of Siblings/Spouses Aboard
+parch Number of Parents/Children Aboard
+ticket Ticket Number
+fare Passenger Fare (British pound)
+cabin Cabin
+embarked Port of Embarkation (C = Cherbourg; Q = Queenstown; S = Southampton)
+boat Lifeboat
+body Body Identification Number
+home.dest Home/Destination
 
-# Pclass Passenger Class (1 = 1st; 2 = 2nd; 3 = 3rd)
-# survival Survival (0 = No; 1 = Yes)
-# name Name
-# sex Sex
-# age Age
-# sibsp Number of Siblings/Spouses Aboard
-# parch Number of Parents/Children Aboard
-# ticket Ticket Number
-# fare Passenger Fare (British pound)
-# cabin Cabin
-# embarked Port of Embarkation (C = Cherbourg; Q = Queenstown; S = Southampton)
-# boat Lifeboat
-# body Body Identification Number
-# home.dest Home/Destination
-
-
+'''
 
 df = pd.read_excel('titanic.xls')
 #print(df.head())
@@ -61,6 +61,27 @@ def handle_non_numerical_data(df):
 
 
 df = handle_non_numerical_data(df)
+
+# df.drop(['ticket'],1,inplace=True) #cant drop it has some impact
+df.drop(['boat','sex'],1,inplace=True) #its your wish after seeing data you can decide the importance of that column
 print(df.head())
+X = np.array(df.drop(['survived'],1).astype(float))
+X = preprocessing.scale(X)  #it will increase accuracy
+y = np.array(df['survived'])
+
+clf = KMeans(n_clusters=2)
+clf.fit(X)
 
 
+correct =  0
+
+#for unsupervised learning
+for i in range(len(X)):
+    predict_me = np.array(X[i].astype(float))
+    predict_me = predict_me.reshape(-1,len(predict_me))
+    prediction = clf.predict(predict_me)
+
+    if prediction[0] == y[i]:
+        correct+=1
+
+print(correct/len(X))
