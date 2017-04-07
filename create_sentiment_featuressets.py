1b806bc072169e8cbb34f8df3e93f40c675324ea
Mukul <mukulkhandelwal1997@gmail.com>
2017-04-07 16:47:13 +0530

    Processing our own Data p48

---
+++ create_sentiment_featuressets.py
@@ -0,0 +1,34 @@
+
+# [chair, table, spoon, television]
+
+# I pulled the chair up to the table
+
+# [0 0 0 0] -> [1 1 0 0]  chair and table comes
+#language tokenization
+
+import nltk
+from nltk.tokenize import word_tokeknize
+# i pulled the cahir up ..
+# [ i, pulled, the , chair, up  ..]  tokenize the words word tokenize
+
+from nltk.stem import WordNetLemmatizer
+import numpy as np
+import random
+import pickle 
+from collections import Counter
+
+
+lemmatizer = WordNetLemmatizer()
+hm_lines = 10000000
+
+
+
+
+
+
+
+
+
+
+
+
